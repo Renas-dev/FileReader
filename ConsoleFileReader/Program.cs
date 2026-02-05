c8d5f8c2d0a7e4c5608169ea3409f879cd2379d0
Renas Khalil <renask.dev@gmail.com>
2026-02-05 14:23:02 +0100

    stage 1, asking / adding user input

--- ConsoleFileReader/Program.cs
+++ ConsoleFileReader/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, please provide the path of the file:");
+
+string? pathLocation = Console.ReadLine();
+
+while (string.IsNullOrWhiteSpace(pathLocation))
+{
+    Console.WriteLine("Invalid input. Please enter a non-empty file path:");
+    pathLocation = Console.ReadLine();
+}
+
+Console.WriteLine($"The given path location is: {pathLocation}");
