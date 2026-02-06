5eceb7be44b2e7acf141e32f930e90cde396ee5e
Renas Khalil <renask.dev@gmail.com>
2026-02-06 16:19:51 +0100

    feat: implement path validation and refactor main program logic

--- ConsoleFileReader/Program.cs
+++ ConsoleFileReader/Program.cs
@@ -1,31 +1,14 @@
 ﻿using System;
 using System.IO;
+using ConsoleFileReaders.Helpers;
 
 class Program
 {
     static void Main()
     {
-        string? path;
-        while (true)
-        {
-            Console.WriteLine("Hello, please provide the path of the file:");
-            string? pathLocation = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(pathLocation))
-            {
-                Console.WriteLine("Invalid input. Please enter a non-empty file path:");
-                continue;
-            }
-            if (!File.Exists(pathLocation))
-            {
-                Console.WriteLine("File not found. Please enter a valid existing file path.");
-                continue;
-            }
-            path = pathLocation;
-            Console.WriteLine($"The given path location exists: {path}");
-            break;
-        }
-        Console.WriteLine(path);
-        string contents = File.ReadAllText(path);
-        Console.WriteLine(contents);
+        string path = PathValidator.GetPath();
+        ReadFile.FileReader(path);
+        LineCounter.DisplayFileLines(path);
     }
 }
+///Users/renas/Desktop/Ebri ProjectBeheer/meower.txt
