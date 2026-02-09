621b13e3c018e21fb35f6a47d6a58c4f2a31e308
Renas Khalil <renask.dev@gmail.com>
2026-02-09 14:25:33 +0100

    feat: refactor menu display logic and improve user interaction

--- ConsoleFileReader/Helpers/RunMenu.cs
+++ ConsoleFileReader/Helpers/RunMenu.cs
@@ -6,20 +6,35 @@ namespace ConsoleFileReaders.Helpers
 {
     class InitializeMenu
     {
-       public static void StartMenu()
+        public static void PrintMenu()
+        {
+            Console.WriteLine("Please choose 1 to print all the text, or 2 to count all the lines or q to exit");
+        }
+        public static void StartMenu()
         {
             string path = PathValidator.GetPath();
-            Console.WriteLine("Please choose 1 to print all the text, or 2 to count all the text");
-            if (File.Exists(path))
+            PrintMenu();
+            while (true)
             {
                 string? input = Console.ReadLine();
                 if (input == "1")
                 {
                     ReadFile.ReadContents(path);
+                    PrintMenu();
                 }
                 else if (input == "2")
                 {
                     LineCounter.CountLines(path);
+                    PrintMenu();
+                }
+                else if (input == "q")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please enter 1, 2, or q.");
+                    PrintMenu();
                 }
             }
         }
