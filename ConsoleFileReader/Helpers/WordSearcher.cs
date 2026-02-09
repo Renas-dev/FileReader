361fcf3139b844eae170bf6839d4c337919c4efd
Renas Khalil <renask.dev@gmail.com>
2026-02-09 16:16:26 +0100

    feat: add word search functionality to the menu and implement WordSearcher class

---
+++ ConsoleFileReader/Helpers/WordSearcher.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+namespace ConsoleFileReaders.Helpers
+{
+    class WordSearcher
+    {
+        public static void FindMatchingLines(string path)
+        {
+            Console.WriteLine("Give a word to lookup");
+            string toSearch = Console.ReadLine()?.Trim() ?? string.Empty;
+            bool foundAny = false;
+            if (string.IsNullOrWhiteSpace(toSearch))
+            {
+                Console.WriteLine("Please type a proper word in");
+                return;
+            }
+            foreach (string line in File.ReadLines(path))
+            {
+                if (line.Contains(toSearch))
+                {
+                    Console.WriteLine(line);
+                    foundAny = true;
+                }
+            }
+            if (foundAny == false)
+            {
+                Console.WriteLine($"word {toSearch} is not present in file");
+            }
+        }
+    }
+}
